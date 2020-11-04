93e167b5870fe9cd99da16d6d24af2b7ed57f01a
Marken <27118779+Marken2808@users.noreply.github.com>
2020-11-04 01:46:46 +0000

    wrong square root

--- CryPract_3/src/ErrorCorrectionBCH/BCHdecoder.java
+++ CryPract_3/src/ErrorCorrectionBCH/BCHdecoder.java
@@ -13,30 +13,65 @@ import java.util.Arrays;
  */
 public class BCHdecoder {
 
-    private int[] s = new int[4];
-    private int[] d;
-    private double[] pqr = new double[3];
+    private final int[] s = new int[4];
+    private final int[] d;
+    final int[] pqr = new int[3];
     public BCHdecoder(int[] d) {
         this.d = d;
-        s[0] = (d[0]+  d[1]+  d[2]+  d[3]+  d[4]+  d[5]+  d[6]+  d[7]+  d[8]+   d[9])%11;
-        s[1] = (d[0]+2*d[1]+3*d[2]+4*d[3]+5*d[4]+6*d[5]+7*d[6]+8*d[7]+9*d[8]+10*d[9])%11;
-        s[2] = (d[0]+4*d[1]+9*d[2]+5*d[3]+3*d[4]+3*d[5]+5*d[6]+9*d[7]+4*d[8]+   d[9])%11;
-        s[3] = (d[0]+8*d[1]+5*d[2]+9*d[3]+4*d[4]+7*d[5]+2*d[6]+6*d[7]+3*d[8]+10*d[9])%11;
+        this.s[0] = (d[0]+  d[1]+  d[2]+  d[3]+  d[4]+  d[5]+  d[6]+  d[7]+  d[8]+   d[9]) %11;
+        this.s[1] = (d[0]+2*d[1]+3*d[2]+4*d[3]+5*d[4]+6*d[5]+7*d[6]+8*d[7]+9*d[8]+10*d[9]) %11;
+        this.s[2] = (d[0]+4*d[1]+9*d[2]+5*d[3]+3*d[4]+3*d[5]+5*d[6]+9*d[7]+4*d[8]+   d[9]) %11;
+        this.s[3] = (d[0]+8*d[1]+5*d[2]+9*d[3]+4*d[4]+7*d[5]+2*d[6]+6*d[7]+3*d[8]+10*d[9]) %11;
 //        System.out.println(Arrays.toString(s));
     } 
-    
-    public double[] calcPQR(){
-        pqr[0] = Math.pow(s[1], 2) - s[0]*s[2];
-        pqr[1] = s[0]*s[3] - s[1]*s[2];
-        pqr[2] = Math.pow(s[2], 2) - s[1]*s[3];
 
-        return pqr ;
+    BCHdecoder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int[] calcPQR(){
+        this.pqr[0] = Math.floorMod(s[1]*s[1] - s[0]*s[2] ,11);
+        this.pqr[1] = Math.floorMod(s[0]*s[3] - s[1]*s[2] ,11);
+        this.pqr[2] = Math.floorMod(s[2]*s[2] - s[1]*s[3] ,11);
+
+        return this.pqr ;
+    }
+    
+    public String check(){
+        
+        int cS = 0, cP = 0;
+        for(int i=0; i<pqr.length; i++){
+            if(pqr[i] == 0){
+//                System.out.println("pqr: "+i);
+                cP++;
+            }
+        }
+        for (int i=0; i<s.length; i++){
+            if(s[i] == 0){
+//                System.out.println("s: "+i);
+                cS++;
+            }
+        }
+        
+        
+        
+        if(cS==s.length){
+            return "no error";
+        }
+        else if(cP==pqr.length ){
+            return "1 error at pos = "+(s[1]/s[0])+" & mag = "+(s[0]);
+        } else {
+            int i = Math.floorMod(((-pqr[1] + (int)Math.sqrt(pqr[1]*pqr[1]-4*pqr[0]*pqr[2])) / (2*pqr[0])) ,11);
+            int j = Math.floorMod((-pqr[1] - (int)Math.sqrt(pqr[1]*pqr[1]-4*pqr[0]*pqr[2])) / (2*pqr[0]) ,11);
+            return "2 errors at i = "+i+" & j = " +j;
+        }
     }
     
     public void call(){
         System.out.println(" d[]: " +Arrays.toString(d));
         System.out.println(" S[]: " +Arrays.toString(s));
-        System.out.println("calc: " + Arrays.toString(calcPQR()));
+        System.out.println("pqr[]: " + Arrays.toString(calcPQR()));
+        System.out.println("check: " +check());
     }
     
     
--- CryPract_3/src/ErrorCorrectionBCH/BCHmain.java
+++ CryPract_3/src/ErrorCorrectionBCH/BCHmain.java
@@ -17,19 +17,40 @@ public class BCHmain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
         
-        int d[];
-        Scanner sc = new Scanner(System.in);
-//        d = new int[6]; // for BCHgenerator
-        d = new int[10];
-        for(int i=0; i<d.length; i++) d[i]=sc.nextInt();
         
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();             // int < long, but String can start with "0"
+        String[] temp = input.split("");
+        int d[] = new int[temp.length];
+        for(int i=0; i<temp.length; i++) {
+                d[i] = Integer.parseInt(temp[i]); 
+        }
+        
+        
+        
+//        System.out.println(" d[]: " +Arrays.toString(d));
+//        System.out.println(" input: " +input);
+//        System.out.println(" temp: " +Arrays.toString(temp));
 //        BCHgenerator digit = new BCHgenerator(d);
 //        int[] new_d = digit.addDigit();
         BCHdecoder dec = new BCHdecoder(d);
         dec.call();
+        int[] pqr = dec.pqr;
+        int P = pqr[0];
+        int Q = pqr[1];
+        int R = pqr[2];
+        
+        Modular m = new Modular();
+        int sqr = m.checkSqr(Q*Q+4*P*R);
+        int i = m.checkNeg(-Q + (int)sqr ) * m.checkPow(2*P,-1);
+        int j = Math.floorMod((-Q - (int)Math.sqrt(Q*Q+4*P*R)) / (2*P) ,11);
+            
+        
+        System.out.println(" sqr: " + sqr);
+        System.out.println(" i: " + i);
 //        String new_d = digit.getNumberic();
         
 //        System.out.println("d[]: " + Arrays.toString(new_d));
